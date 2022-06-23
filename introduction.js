603fc6bd0d2402c71d5d8c72f893535ed30160b2
rasheey97-alt <rasheey97@yahoo.com>
2022-06-24 00:43:51 +0100

    introduction to Javascript

---
+++ introduction.js
@@ -0,0 +1,10 @@
+// Name, Height, and Country PRINTED on the screen
+
+const info = {
+  name: "Uthman Rasheed",
+  height: "1.6 ft",
+  country: "Nigeria",
+};
+
+
+alert(`Name: ${info.name} \nHeight: ${info.height} \nCountry: ${info.country}`);
